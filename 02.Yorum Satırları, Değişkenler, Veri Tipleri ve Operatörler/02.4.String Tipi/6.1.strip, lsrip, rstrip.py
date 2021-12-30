b84ee3a6d6ce8b5f797bc531784856726b030358
kbala42 <kbala42@aim.com>
2021-12-30 22:07:44 +0300

    Gunceleme_30.12.2021_2

--- 02.Yorum Satırları, Değişkenler, Veri Tipleri ve Operatörler/02.4.String Tipi/6.1.strip, lsrip, rstrip.py
+++
@@ -1,8 +0,0 @@
-metin1 = 'Merhaba '
-metin2 = 'Mars'
-print (metin1) # karakter dizisinin tamamını yazar
-print (metin1 * 2) # karakter dizisini 2 defa yazar
-print (metin1 + metin2) # iki karakter dizisini birleştirir
-print ('metin1 adlı değişkendeki değerin uzunluğu:', len(metin1))#boşluk bir karakter
-
-
