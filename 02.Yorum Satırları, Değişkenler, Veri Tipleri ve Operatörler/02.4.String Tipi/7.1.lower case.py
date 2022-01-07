dbaced85d7dd0e69dbf4b5d4e8d8498bcb4caa19
kbala42 <71403501+kbala42@users.noreply.github.com>
2022-01-07 17:32:56 +0300

    upt_07.01.22.1

---
+++ 02.Yorum Satırları, Değişkenler, Veri Tipleri ve Operatörler/02.4.String Tipi/7.1.lower case.py
@@ -0,0 +1,10 @@
+print('-----------------------------------------------')
+print('lower() fonksiyonu bütün metni küçük harf yapar')
+print('-----------------------------------------------')
+metin1 = 'MERHABA Python'
+print(metin1.lower())
+print()
+
+
+
+
---
+++ 02.Yorum Satırları, Değişkenler, Veri Tipleri ve Operatörler/02.4.String Tipi/7.3.capital.py
@@ -0,0 +1,11 @@
+print('-------------------------------------------------')
+print('capitalize fonksiyonu metnin ilk harfi dışındaki'
+      ' harleri küçük harf yapar ')
+print('------------------------------------------------')
+metin1 = 'MERHABA Python'
+print(metin1.capitalize())
+print()
+
+
+
+
