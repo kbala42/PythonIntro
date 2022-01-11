c2f07b20126d938630fb78bbf583a231f2fc5d00
kbala42 <71403501+kbala42@users.noreply.github.com>
2022-01-11 14:36:49 +0300

    upt_11.01.2022_2

---
+++ 05.Tuplelar (demetler)/3.2.Tuple cozumleme.py
@@ -0,0 +1,13 @@
+print('----------------------------------------------------------------tw:@tek_elo')
+print('Tuple oluşturduktan sonra tekrar tuple adına içeriği atarsak\n'
+      'Tuple çözümlenmiş olur')
+print('---------------------------------------------------------------------------')
+haftaninGunleri=('Pazartesi','Sali','Çarşamba','Perşembe','Cuma')
+
+(gun1, gun2, gun3, gun4, gun5) = haftaninGunleri
+
+print(gun1)
+print(type(gun1))
+
+print(gun2)
+print(type(gun2))
